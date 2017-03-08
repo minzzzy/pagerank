daf65ed03fce3c87ef3ded65279f192dc930dc30
minzykim <minpjh@gmail.com>
2017-03-08 23:17:39 +0900

    modify function name

--- crawl.py
+++ crawl.py
@@ -5,70 +5,126 @@ class Crawling(object):
     def __init__(self):
         self.index = {}
         self.graph = {}
+        self.cache = {
+           'http://udacity.com/cs101x/urank/index.html': """<html>
+                                                        <body>
+                                                        <h1>dave's cooking algorithms</h1>
+                                                        <p>
+                                                        here are my favorite recipies:
+                                                        <ul>
+                                                        <li> <a href="http://udacity.com/cs101x/urank/hummus.html">hummus recipe</a>
+                                                        <li> <a href="http://udacity.com/cs101x/urank/arsenic.html">world's best hummus</a>
+                                                        <li> <a href="http://udacity.com/cs101x/urank/kathleen.html">kathleen's hummus recipe</a>
+                                                        </ul>
+                                                        for more expert opinions, check out the
+                                                        <a href="http://udacity.com/cs101x/urank/nickel.html">nickel chef</a>
+                                                        and <a href="http://udacity.com/cs101x/urank/zinc.html">zinc chef</a>.
+                                                        </body>
+                                                        </html>
+                                                        """,
 
-    #def record_user_click(self,index,keyword,url):
-    #    return
+           'http://udacity.com/cs101x/urank/zinc.html': """<html>
+                                                        <body>
+                                                        <h1>the zinc chef</h1>
+                                                        <p>
+                                                        i learned everything i know from
+                                                        <a href="http://udacity.com/cs101x/urank/nickel.html">the nickel chef</a>.
+                                                        </p>
+                                                        <p>
+                                                        for great hummus, try
+                                                        <a href="http://udacity.com/cs101x/urank/arsenic.html">this recipe</a>.
+                                                        </body>
+                                                        </html>
+                                                        """,
 
-    def get_page(self,url):
-        try:
-            if url == "http://www.udacity.com/cs101x/index.html":
-                return '''<html> <body> This is a test page for learning to crawl!
-    <p> It is a good idea to
-    <a href="http://www.udacity.com/cs101x/crawling.html">
-    learn to crawl</a> before you try to
-    <a href="http://www.udacity.com/cs101x/walking.html">walk</a> or
-    <a href="http://www.udacity.com/cs101x/flying.html">fly</a>.</p></body></html>'''
+           'http://udacity.com/cs101x/urank/nickel.html': """<html>
+                                                        <body>
+                                                        <h1>the nickel chef</h1>
+                                                        <p>
+                                                        this is the
+                                                        <a href="http://udacity.com/cs101x/urank/kathleen.html">
+                                                        best hummus recipe!
+                                                        </a>
+                                                        </body>
+                                                        </html>
+                                                        """,
 
-            elif url == "http://www.udacity.com/cs101x/crawling.html":
-                return '''<html> <body> I have not learned to crawl yet, but I am
-    quite good at  <a href="http://www.udacity.com/cs101x/kicking.html">kicking</a>.
-    </body> </html>'''
+           'http://udacity.com/cs101x/urank/kathleen.html': """<html>
+                                                        <body>
+                                                        <h1>
+                                                        kathleen's hummus recipe
+                                                        </h1>
+                                                        <p>
+                                                        <ol>
+                                                        <li> open a can of garbonzo beans.
+                                                        <li> crush them in a blender.
+                                                        <li> add 3 tablesppons of tahini sauce.
+                                                        <li> squeeze in one lemon.
+                                                        <li> add salt, pepper, and buttercream frosting to taste.
+                                                        </ol>
+                                                        </body>
+                                                        </html>
+                                                        """,
 
-            elif url == "http://www.udacity.com/cs101x/walking.html":
-                return '''<html> <body> I cant get enough
-    <a href="http://www.udacity.com/cs101x/index.html">crawling</a>!</body></html>'''
+           'http://udacity.com/cs101x/urank/arsenic.html': """<html>
+                                                        <body>
+                                                        <h1>
+                                                        the arsenic chef's world famous hummus recipe
+                                                        </h1>
+                                                        <p>
+                                                        <ol>
+                                                        <li> kidnap the <a href="http://udacity.com/cs101x/urank/nickel.html">nickel chef</a>.
+                                                        <li> force her to make hummus for you.
+                                                        </ol>
+                                                        </body>
+                                                        </html>
+                                                        """,
 
-            elif url == "http://www.udacity.com/cs101x/flying.html":
-                return '<html><body>The magic words are Squeamish Ossifrage!</body></html>'
-        except:
-            return ""
-        return ""
+           'http://udacity.com/cs101x/urank/hummus.html': """<html>
+                                                        <body>
+                                                        <h1>
+                                                        hummus recipe
+                                                        </h1>
+                                                        <p>
+                                                        <ol>
+                                                        <li> go to the store and buy a container of hummus.
+                                                        <li> open it.
+                                                        </ol>
+                                                        </body>
+                                                        </html>
+                                                        """,
+        }
 
-    def remove_tags(self, string):
-        start, end = 0, 0
-        while True:
-            start = string.find('<')
-            if start == -1:
-                return string.split()
-            end = string.find('>', start)
-            string = string[:start] + ' ' + string[end+1:]
 
+    ## web crawling
     def crawl_web(self, seed):
         tocrawl = [seed]
         crawled = []
         while tocrawl:
             page = tocrawl.pop()
             if page not in crawled:
-                content = self.get_page(page)
-                self.add_page_to_index(self.index, page, content)
-                outlinks = self.get_all_links(content)
-                self.union(tocrawl, outlinks)
+                content = self._get_page(page)
+                self._add_page_to_index(self.index, page, content)
+                outlinks = self._get_all_links(content)
+                self._union(tocrawl, outlinks)
                 self.graph[page] = outlinks
                 crawled.append(page)
         return self.index, self.graph
 
-    def add_to_index(self,index, keyword, url):
-        if keyword in index:
-            index[keyword].append(url)
-        else:
-            index[keyword] = [url]
+    def _get_page(self,url):
+        if url in self.cache:
+            return self.cache[url]
+        return ""
 
-    def add_page_to_index(self, index, url, content):
+    def _add_page_to_index(self, index, url, content):
         words = content.split()
         for word in words:
-            self.add_to_index(index, word, url)
+            if word in index:
+                index[word].append(url)
+            else:
+                index[word] = [url]
 
-    def get_next_target(self, page):
+    def _get_next_target(self, page):
         start_link = page.find('<a href=')
         if start_link == -1:
             return None, 0
@@ -77,10 +133,10 @@ class Crawling(object):
         url = page[start_quote+1:end_quote]
         return url, end_quote
 
-    def get_all_links(self, page):
+    def _get_all_links(self, page):
         links = []
         while True:
-            url, endpos = self.get_next_target(page)
+            url, endpos = self._get_next_target(page)
             if url:
                 links.append(url)
                 page = page[endpos:]
@@ -88,16 +144,18 @@ class Crawling(object):
                 break
         return links
 
-    def union(self, a, b):
+    def _union(self, a, b):
         for e in b:
             if e not in a:
                 a.append(e)
 
+    ## look up the keyword in index
     def lookup(self, index, keyword):
         if keyword not in index:
             return index[keyword]
-        return None
+        return none
 
+    ## compute page rank using graph
     def compute_ranks(self, graph, k):
         d = 0.8
         numloops = 2
@@ -112,13 +170,13 @@ class Crawling(object):
                 newrank = (1 - d) / npages
                 for node in graph:
                     if page in graph[node]:
-                        if not self.is_reciprocal_link(graph, node, page, k):
+                        if not self._is_reciprocal_link(graph, node, page, k):
                             newrank = newrank + d*ranks[node]/len(graph[node])
                 newranks[page] = newrank
             ranks = newranks
         return ranks
 
-    def is_reciprocal_link(self, graph, source, destination, k):
+    def _is_reciprocal_link(self, graph, source, destination, k):
         if k == 0:
             if source == destination:
                 return True
@@ -126,11 +184,23 @@ class Crawling(object):
         if source in graph[destination]:
             return True
         for node in graph[destination]:
-            if self.is_reciprocal_link(graph, source, node, k-1):
+            if self._is_reciprocal_link(graph, source, node, k-1):
                 return True
         return False
 
-    def quicksort(self, l):
+    ## sort ascending
+    def ordered_search(self, index, ranks, keyword):
+        dic = {}
+        res = []
+        for e in index[keyword]:
+            if e in ranks:
+                dic[ranks[e]] = e
+        key = self._quicksort(dic.keys())
+        for i in key:
+            res.append(dic[i])
+        return res
+
+    def _quicksort(self, l):
         if len(l) <= 1:
             return l
         pivot = l[len(l)/2]
@@ -144,16 +214,17 @@ class Crawling(object):
                 equal.append(e)
             else:
                 bigger.append(e)
-        return self.quicksort(bigger) + equal + self.quicksort(smaller)
+        return self._quicksort(bigger) + equal + self._quicksort(smaller)
+    
+## comment
+#    def remove_tags(self, string):
+#        start, end = 0, 0
+#        while True:
+#            start = string.find('<')
+#            if start == -1:
+#                return string.split()
+#            end = string.find('>', start)
+#            string = string[:start] + ' ' + string[end+1:]
 
-    ## Ascending
-    def ordered_search(self, index, ranks, keyword):
-        dic = {}
-        res = []
-        for e in index[keyword]:
-            if e in ranks:
-                dic[ranks[e]] = e
-        key = self.quicksort(dic.keys())
-        for i in key:
-            res.append(dic[i])
-        return res
+#    def record_user_click(self,index,keyword,url):
+#        return
--- web.py
+++ web.py
@@ -3,8 +3,8 @@
 from crawl import Crawling
 
 mingrome = Crawling()
-index, graph = mingrome.crawl_web('http://www.udacity.com/cs101x/index.html')
+index, graph = mingrome.crawl_web('http://udacity.com/cs101x/urank/index.html')
 ranks = mingrome.compute_ranks(graph, 0)
 #print index
 print ranks
-print mingrome.ordered_search(index, ranks, 'for')
+#print mingrome.ordered_search(index, ranks, 'for')
