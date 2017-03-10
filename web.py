2b5dba0e8ef3549733f52f9df76d26f071caa41e
minzzzy <minpjh@gmail.com>
2017-03-10 14:18:22 +0900

    Update web.py

--- web.py
+++ web.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
 from crawl import Crawling
+import pprint
 
 mingrome = Crawling()
 index, graph = mingrome.crawl_web('http://udacity.com/cs101x/urank/index.html')
 ranks = mingrome.compute_ranks(graph, 0)
-#print index
-print ranks
-#print mingrome.ordered_search(index, ranks, 'for')
+
+# search 'for'
+print mingrome.ordered_search(index, ranks, 'for')
