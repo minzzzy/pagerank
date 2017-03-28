336db333f2039e5c356a18d15b28f137580484d6
minzzzy <minpjh@gmail.com>
2017-03-28 14:58:22 +0900

    Update make_search_engine.py

--- make_search_engine.py
+++ make_search_engine.py
@@ -8,4 +8,4 @@ index, graph = mingle.crawl_web('http://udacity.com/cs101x/urank/index.html')
 ranks = mingle.compute_ranks(graph, 0)
 
 # search 'for'
-print mingrome.ordered_search(index, ranks, 'for')
+print mingle.ordered_search(index, ranks, 'for')
