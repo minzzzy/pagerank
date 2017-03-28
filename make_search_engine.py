18457edbdbbf45449d3f3c59fd9cfdea44552bd3
minzzzy <minpjh@gmail.com>
2017-03-28 14:57:54 +0900

    Update make_search_engine.py

--- make_search_engine.py
+++ make_search_engine.py
@@ -3,9 +3,9 @@
 from Search_engine import Search_engine
 import pprint
 
-mingrome = Search_engine()
-index, graph = mingrome.crawl_web('http://udacity.com/cs101x/urank/index.html')
-ranks = mingrome.compute_ranks(graph, 0)
+mingle = Search_engine()
+index, graph = mingle.crawl_web('http://udacity.com/cs101x/urank/index.html')
+ranks = mingle.compute_ranks(graph, 0)
 
 # search 'for'
 print mingrome.ordered_search(index, ranks, 'for')
